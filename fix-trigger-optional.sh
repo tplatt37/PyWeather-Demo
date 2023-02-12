cccce0b14dce0e0e196d4605847e0d6aac1cd9c6
TR Platt <trplatt@amazon.com>
2023-02-12 13:48:02 +0000

    Trigger is as good as it is going to get

---
+++ fix-trigger-optional.sh
@@ -0,0 +1,15 @@
+#
+# NOTE: When using SAM, it's not possible to get the API Gateway
+# trigger to show in the Console properly.
+# But, you can run this command
+# 
+# Replace zzz with the Function name
+# Replace xxx with the AccountId, and yyy with the Rest API Id
+#
+
+aws lambda add-permission \
+--function-name zzzzz \
+--action lambda:InvokeFunction \
+--statement-id test \
+--principal apigateway.amazonaws.com \
+--source-arn arn:aws:execute-api:us-west-2:xxx:yyyyy/*/GET/weather
