8c089f8a8748639c15fe0629ce71001effb993f0
TR Platt <trplatt@amazon.com>
2023-10-29 10:46:08 +0000

    improvements

--- fix-trigger-optional.sh
+++ fix-trigger-optional.sh
@@ -1,7 +1,7 @@
 #
 # NOTE: When using SAM, it's not possible to get the API Gateway
-# trigger to show in the Console properly.
-# But, you can run this command
+# trigger to show in the Console properly. There's a glaring red error message
+# But, you can run this command to get rid of it!
 # 
 # Replace zzz with the Function name
 # Replace xxx with the AccountId, and yyy with the Rest API Id
@@ -9,10 +9,28 @@
 
 # Pull this info from the stack exports...
 
+# Retrieve CloudFormation Stack export named "PyWeatherDemoLambdaName"
+LAMBDA_NAME=$(aws cloudformation describe-stacks \
+--stack-name PyWeather-Demo \
+--query 'Stacks[].Outputs[?OutputKey==`PyWeatherDemoLambdaName`].OutputValue' \
+--output text)
+echo "LAMBDA_NAME=$LAMBDA_NAME"
+
+API=$(aws cloudformation describe-stacks \
+--stack-name PyWeather-Demo \
+--query 'Stacks[].Outputs[?OutputKey==`PyWeatherDemoRestApi`].OutputValue' \
+--output text)
+echo "API=$API"
+
+ACCOUNT_ID=$(aws cloudformation describe-stacks \
+--stack-name PyWeather-Demo \
+--query 'Stacks[].Outputs[?OutputKey==`PyWeatherDemoAccountId`].OutputValue' \
+--output text)
+echo "ACCOUNT_ID=$ACCOUNT_ID"
 
 aws lambda add-permission \
---function-name zzzzz \
+--function-name $LAMBDA_NAME \
 --action lambda:InvokeFunction \
 --statement-id test \
 --principal apigateway.amazonaws.com \
---source-arn arn:aws:execute-api:us-west-2:xxx:yyyyy/*/GET/weather
+--source-arn arn:aws:execute-api:us-west-2:$ACCOUNT_ID:$API/*/GET/weather
--- uninstall.sh
+++ uninstall.sh
@@ -18,11 +18,19 @@ aws s3 rm s3://$UTILITY_BUCKET --recursive
 
 
 # Delete this stack first!
+# But make sure it's not in UPDATE_IN_PROGRESS, because that causes issues.. (it can't be deleted)
 STACK_NAME=$PREFIX
+
+# Check if stack is in UPDATE_IN_PROGRES
+STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].StackStatus" --output text)
+if [ "$STACK_STATUS" == "UPDATE_IN_PROGRESS" ]; then
+  echo "Stack ($STACK_NAME) is in UPDATE_IN_PROGRESS, cannot delete it yet. Please wait for it to finish and try again"
+  exit 1
+fi
+
 echo "Deleting ($STACK_NAME) ..."
 aws cloudformation delete-stack --stack-name $STACK_NAME
 aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME 
-
 
 STACK_NAME=$PREFIX-Pipeline
 echo "Deleting ($STACK_NAME) ..."
