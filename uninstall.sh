87700902bdb49d2b3a0e9910b5264d582b5da31c
TR Platt <trplatt@amazon.com>
2023-10-29 00:46:20 +0000

    have to uninstall -repo stack also

--- uninstall.sh
+++ uninstall.sh
@@ -29,3 +29,7 @@ echo "Deleting ($STACK_NAME) ..."
 aws cloudformation delete-stack --stack-name $STACK_NAME
 aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME 
 
+STACK_NAME=$PREFIX-repo
+echo "Deleting ($STACK_NAME) ..."
+aws cloudformation delete-stack --stack-name $STACK_NAME
+aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME 
